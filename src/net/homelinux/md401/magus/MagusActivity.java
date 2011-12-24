c45f4a34778ca45f26efea348a8d99a894223773
Michael Donaghy <Michael@.(none)>
2011-12-24 12:40:46 +0000

    initial eclipse thing

---
+++ src/net/homelinux/md401/magus/MagusActivity.java
@@ -0,0 +1,13 @@
+package net.homelinux.md401.magus;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MagusActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
