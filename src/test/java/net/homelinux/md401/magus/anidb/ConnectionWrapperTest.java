f7dd7f1cfb4b689be55faef492dcd8714bbc10fe
Michael Donaghy <md401@srcf.ucam.org>
2011-12-26 03:51:33 +0000

    more replays, still no good

--- src/test/java/net/homelinux/md401/magus/anidb/ConnectionWrapperTest.java
+++ src/test/java/net/homelinux/md401/magus/anidb/ConnectionWrapperTest.java
@@ -1,5 +1,7 @@
 package net.homelinux.md401.magus.anidb;
 
+import java.util.List;
+
 import net.anidb.udp.UdpConnection;
 import net.anidb.udp.UdpConnectionFactory;
 
@@ -14,6 +16,7 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import com.google.common.base.Function;
+import com.google.common.collect.Lists;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(UdpConnectionFactory.class)
@@ -23,14 +26,21 @@ public class ConnectionWrapperTest {
 	private UdpConnection connection;
 	private MockFunction function;
 	private ConnectionWrapper wrapper;
+	private final List<Object> mocks = Lists.newArrayList();
+	
+	private <T> T mock(Class<T> tclass) {
+		T ret = createMock(tclass);
+		mocks.add(ret);
+		return ret;
+	}
 
 	@Before
 	public void setUp() {
 		PowerMock.mockStatic(UdpConnectionFactory.class);
-		factory = createMock(UdpConnectionFactory.class);
+		factory = mock(UdpConnectionFactory.class);
 		expect(UdpConnectionFactory.getInstance()).andReturn(factory);
-		connection = createMock(UdpConnection.class);
-		function = createMock(MockFunction.class);
+		connection = mock(UdpConnection.class);
+		function = mock(MockFunction.class);
 		PowerMock.replay(UdpConnectionFactory.class);
 		wrapper = new ConnectionWrapper();
 	}
@@ -41,8 +51,16 @@ public class ConnectionWrapperTest {
 		expect(factory.connect(anyInt())).andReturn(connection);
 		expect(function.apply(connection)).andReturn(expected);
 		connection.close();
-		PowerMock.replayAll();
+		replay();
 		assertThat(wrapper.perform(function)).isEqualTo(expected);
+		verify();
+	}
+
+	private void verify() {
 		PowerMock.verifyAll();
+	}
+
+	private void replay() {
+		PowerMock.replayAll(mocks.toArray());
 	}
 }
