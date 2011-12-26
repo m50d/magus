74f45d5a54110271c7d6f5e8dd07965296e60e45
Michael Donaghy <md401@srcf.ucam.org>
2011-12-26 02:50:26 +0000

    Progress towards a test

--- src/test/java/net/homelinux/md401/magus/anidb/ConnectionWrapperTest.java
+++ src/test/java/net/homelinux/md401/magus/anidb/ConnectionWrapperTest.java
@@ -1,5 +1,23 @@
 package net.homelinux.md401.magus.anidb;
 
-public class ConnectionWrapperTest {
+import net.anidb.udp.UdpConnectionFactory;
 
+import org.easymock.EasyMock;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.powermock.api.easymock.PowerMock;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(UdpConnectionFactory.class)
+public class ConnectionWrapperTest {
+	private UdpConnectionFactory factory;
+
+	@Before
+	public void setUp() {
+		PowerMock.mockStatic(UdpConnectionFactory.class);
+		factory = EasyMock.createMock(UdpConnectionFactory.class);
+		EasyMock.expect(UdpConnectionFactory.getInstance()).andReturn(factory);
+	}
 }
