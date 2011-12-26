36bbb077c53589d09fc738365181a8124c6fe1a0
Michael Donaghy <md401@srcf.ucam.org>
2011-12-26 03:04:05 +0000

    Test that should fail

--- src/test/java/net/homelinux/md401/magus/anidb/ConnectionWrapperTest.java
+++ src/test/java/net/homelinux/md401/magus/anidb/ConnectionWrapperTest.java
@@ -1,23 +1,46 @@
 package net.homelinux.md401.magus.anidb;
 
+import net.anidb.udp.UdpConnection;
 import net.anidb.udp.UdpConnectionFactory;
 
-import org.easymock.EasyMock;
+import static org.easymock.EasyMock.*;
+import static org.fest.assertions.Assertions.assertThat;
+
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.api.easymock.PowerMock;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import com.google.common.base.Function;
+
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(UdpConnectionFactory.class)
 public class ConnectionWrapperTest {
+	private static interface MockFunction extends Function<UdpConnection, Object>{}
 	private UdpConnectionFactory factory;
+	private UdpConnection connection;
+	private MockFunction function;
+	private ConnectionWrapper wrapper;
 
 	@Before
 	public void setUp() {
 		PowerMock.mockStatic(UdpConnectionFactory.class);
-		factory = EasyMock.createMock(UdpConnectionFactory.class);
-		EasyMock.expect(UdpConnectionFactory.getInstance()).andReturn(factory);
+		factory = createMock(UdpConnectionFactory.class);
+		expect(UdpConnectionFactory.getInstance()).andReturn(factory);
+		connection = createMock(UdpConnection.class);
+		function = createMock(MockFunction.class);
+		wrapper = new ConnectionWrapper();
+	}
+	
+	@Test 
+	public void invokesSuppliedFunction() throws Exception {
+		Object expected = new Object();
+		expect(factory.connect(anyInt())).andReturn(connection);
+		expect(function.apply(connection)).andReturn(expected);
+		PowerMock.replayAll();
+		assertThat(wrapper.perform(function)).isEqualTo(expected);
+		PowerMock.verifyAll();
 	}
 }
