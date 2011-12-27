ea79b0273bf9a800aa0b0d54d37a860dab8be4d2
Michael Donaghy <md401@srcf.ucam.org>
2011-12-27 19:07:19 +0000

    start at implementation

--- src/main/java/net/homelinux/md401/magus/anidb/ConnectionWrapper.java
+++ src/main/java/net/homelinux/md401/magus/anidb/ConnectionWrapper.java
@@ -1,9 +1,13 @@
 package net.homelinux.md401.magus.anidb;
 
+import net.anidb.File;
 import net.anidb.udp.AniDbException;
 import net.anidb.udp.UdpConnection;
 import net.anidb.udp.UdpConnectionException;
 import net.anidb.udp.UdpConnectionFactory;
+import net.anidb.udp.UdpFileFactory;
+import net.anidb.udp.mask.AnimeFileMask;
+import net.anidb.udp.mask.FileMask;
 
 import com.google.common.base.Function;
 
@@ -37,6 +41,25 @@ public class ConnectionWrapper {
 					throw new RuntimeException(e);
 				}
 				return toInvoke.apply(arg0);
+			}
+		});
+	}
+
+	void add(final String username, final String password, final long fileId) throws UdpConnectionException, AniDbException {
+		performWithAuthentication(username, password, new Function<UdpConnection, Void>() {
+
+			@Override
+			public Void apply(final UdpConnection from) {
+				final UdpFileFactory fileFactory = UdpFileFactory.getInstance(from);
+				File file;
+				try {
+					file = fileFactory.getFile(fileId, FileMask.ALL, AnimeFileMask.ALL);
+				} catch (final UdpConnectionException e) {
+					throw new RuntimeException(e);
+				} catch (final AniDbException e) {
+					throw new RuntimeException(e);
+				}
+				return null;
 			}
 		});
 	}
