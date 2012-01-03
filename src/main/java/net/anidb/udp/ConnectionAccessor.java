4ab7cc48f45f52947de50107617a52455b503af0
Michael Donaghy <md401@srcf.ucam.org>
2012-01-03 19:31:36 +0000

    Move files into libmagus

--- src/main/java/net/anidb/udp/ConnectionAccessor.java
+++
@@ -1,8 +0,0 @@
-package net.anidb.udp;
-
-public final class ConnectionAccessor {
-	private ConnectionAccessor(){}
-	public static UdpResponse communicate(final UdpConnection connection, final UdpRequest request) throws UdpConnectionException {
-		return connection.communicate(request);
-	}
-}
