1284b3626a44e31a8526988151cc205af4123c67
Michael Donaghy <md401@srcf.ucam.org>
2012-01-07 10:52:58 +0000

    progress, hopefully

--- src/main/java/net/homelinux/md401/magus/MagusActivity.java
+++ src/main/java/net/homelinux/md401/magus/MagusActivity.java
@@ -35,44 +35,29 @@ import android.widget.Toast;
 public class MagusActivity extends Activity {
 	protected static final int REQUEST_CODE_PICK_FILE_OR_DIRECTORY = 1;
 	protected static final int REQUEST_CODE_GET_CONTENT = 2;
+	private View username;
+	private View password;
 
-	protected EditText editText;
-	protected TextView textView;
-
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
-        editText = (EditText) findViewById(R.id.file_path);
-        textView = (TextView) findViewById(R.id.info);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
     }
 
 	public void onClickOpenFile(final View view) {
 		openFile();
 	}
 
-	public void onClickGetContent(final View view) {
-		getContent();
-	}
-
     /**
      * Opens the file manager to select a file to open.
      */
     public void openFile() {
-		final String fileName = editText.getText().toString();
-
 		final Intent intent = new Intent(FileManagerIntents.ACTION_PICK_FILE);
-
-		// Construct URI from file name.
-		final File file = new File(fileName);
-		intent.setData(Uri.fromFile(file));
-
-		// Set fancy title and button (optional)
+		intent.setData(Uri.fromFile(new File("")));
 		intent.putExtra(FileManagerIntents.EXTRA_TITLE, "Choose file to add to mylist");
 		intent.putExtra(FileManagerIntents.EXTRA_BUTTON_TEXT, "Add to mylist");
-
 		try {
 			startActivityForResult(intent, REQUEST_CODE_PICK_FILE_OR_DIRECTORY);
 		} catch (final ActivityNotFoundException e) {
@@ -82,23 +67,20 @@ public class MagusActivity extends Activity {
 		}
 	}
 
-    /**
-     * Use GET_CONTENT to open a file.
-     */
-    public void getContent() {
-
-		final Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-		intent.setType("*/*");
-		intent.addCategory(Intent.CATEGORY_OPENABLE);
-
-		try {
-			startActivityForResult(intent, REQUEST_CODE_GET_CONTENT);
-		} catch (final ActivityNotFoundException e) {
-			// No compatible file manager was found.
-			Toast.makeText(this, "No compatible file manager found",
-					Toast.LENGTH_SHORT).show();
-		}
-    }
+//    public void getContent() {
+//
+//		final Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+//		intent.setType("*/*");
+//		intent.addCategory(Intent.CATEGORY_OPENABLE);
+//
+//		try {
+//			startActivityForResult(intent, REQUEST_CODE_GET_CONTENT);
+//		} catch (final ActivityNotFoundException e) {
+//			// No compatible file manager was found.
+//			Toast.makeText(this, "No compatible file manager found",
+//					Toast.LENGTH_SHORT).show();
+//		}
+//    }
 
     /**
      * This is called after the file manager finished.
@@ -106,9 +88,6 @@ public class MagusActivity extends Activity {
 	@Override
 	protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-
-		textView.setText("");
-
 		switch (requestCode) {
 		case REQUEST_CODE_PICK_FILE_OR_DIRECTORY:
 			if (resultCode == RESULT_OK && data != null) {
@@ -116,33 +95,29 @@ public class MagusActivity extends Activity {
 				final Uri fileUri = data.getData();
 				if (fileUri != null) {
 					final String filePath = fileUri.getPath();
-					if (filePath != null)
-						editText.setText(filePath);
+					File file = new File(filePath).getAbsoluteFile();
 				}
 			}
-			break;
-		case REQUEST_CODE_GET_CONTENT:
-			if (resultCode == RESULT_OK && data != null) {
-				String filePath = null;
-				long fileSize = 0;
-				String displayName = null;
-				final Uri uri = data.getData();
-				final Cursor c = getContentResolver().query(uri, new String[] {MediaStore.MediaColumns.DATA,
-					MediaStore.MediaColumns.MIME_TYPE,
-					MediaStore.MediaColumns.DISPLAY_NAME,
-					MediaStore.MediaColumns.SIZE
-				}, null, null, null);
-				if (c != null && c.moveToFirst()) {
-					final int id = c.getColumnIndex(MediaColumns.DATA);
-					if (id != -1)
-						filePath = c.getString(id);
-					displayName = c.getString(2);
-					fileSize = c.getLong(3);
-				}
-				if (filePath != null)
-					editText.setText(filePath);
-//					textView.setText(strFileSize);
-			}
+			return;
+//		case REQUEST_CODE_GET_CONTENT:
+//			if (resultCode == RESULT_OK && data != null) {
+//				String filePath = null;
+//				long fileSize = 0;
+//				String displayName = null;
+//				final Uri uri = data.getData();
+//				final Cursor c = getContentResolver().query(uri, new String[] {MediaStore.MediaColumns.DATA,
+//					MediaStore.MediaColumns.MIME_TYPE,
+//					MediaStore.MediaColumns.DISPLAY_NAME,
+//					MediaStore.MediaColumns.SIZE
+//				}, null, null, null);
+//				if (c != null && c.moveToFirst()) {
+//					final int id = c.getColumnIndex(MediaColumns.DATA);
+//					if (id != -1)
+//						filePath = c.getString(id);
+//					displayName = c.getString(2);
+//					fileSize = c.getLong(3);
+//				}
+//			}
 		}
 	}
 }
