d32ad1caeca3345019e774cc9f72433e9f4d1cb8
Michael Donaghy <md401@srcf.ucam.org>
2012-01-07 12:43:53 +0000

    Organize imports

--- src/main/java/net/homelinux/md401/magus/MagusActivity.java
+++ src/main/java/net/homelinux/md401/magus/MagusActivity.java
@@ -22,13 +22,9 @@ import org.openintents.intents.FileManagerIntents;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.provider.MediaStore.MediaColumns;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
