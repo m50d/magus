4cd0a3a3e6a63454f1e173b8fa52a2e4b2973437
Michael Donaghy <md401@srcf.ucam.org>
2012-06-23 21:06:16 +0100

    Some idea of a service that does stuff

--- src/main/java/net/homelinux/md401/magus/AddToMylistService.java
+++ src/main/java/net/homelinux/md401/magus/AddToMylistService.java
@@ -1,18 +1,22 @@
 package net.homelinux.md401.magus;
 
 import android.app.IntentService;
+import android.app.Notification;
 import android.content.Intent;
 
 public class AddToMylistService extends IntentService {
-	private final FileHandler handler;
-	public AddToMylistService(final FileHandler handler){
+	private final FileHandler handler = new FileHandler();
+	private static final int SCIENTIST = 7;
+	public AddToMylistService(){
 		super("AddFilesToMylist");
-		this.handler = handler;
 	}
 
 	@Override
 	protected void onHandleIntent(Intent intent) {
 		UsernamePasswordFile param = (UsernamePasswordFile) intent.getSerializableExtra("UsernamePasswordFile");
+//		NotificationManager notificationService = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+		Notification notification = new Notification.Builder(this).setContentTitle("Hashing File").getNotification();
+		startForeground(SCIENTIST, notification);
 		String s;
 		try {
 			handler.addFile(param.username, param.password, true, param.file);
