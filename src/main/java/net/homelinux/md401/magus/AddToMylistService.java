05d08017989fe510dc05a1a2f208d76dfdba9498
Michael Donaghy <md401@srcf.ucam.org>
2012-07-03 20:10:37 +0100

    Attempt to add a visible notification

--- src/main/java/net/homelinux/md401/magus/AddToMylistService.java
+++ src/main/java/net/homelinux/md401/magus/AddToMylistService.java
@@ -2,6 +2,7 @@ package net.homelinux.md401.magus;
 
 import android.app.IntentService;
 import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.Intent;
 
 public class AddToMylistService extends IntentService {
@@ -14,7 +15,7 @@ public class AddToMylistService extends IntentService {
 	@Override
 	protected void onHandleIntent(Intent intent) {
 		UsernamePasswordFile param = (UsernamePasswordFile) intent.getSerializableExtra(UsernamePasswordFile.USERNAME_PASSWORD_FILE);
-//		NotificationManager notificationService = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+		NotificationManager notificationService = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 		Notification notification = new Notification.Builder(this).setContentTitle("Hashing File").getNotification();
 		startForeground(SCIENTIST, notification);
 		String s;
@@ -24,6 +25,7 @@ public class AddToMylistService extends IntentService {
 		} catch (FailureException e) {
 			s = e.detail;
 		}
+		notificationService.notify(SCIENTIST, new Notification.Builder(this).setContentText(s).getNotification());
 	}
 
 }
